900b5e67ace349e3ae491c378b140ed932956087
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-12-15 23:39:48 +0000

    Update testCase.js

--- JavaScript/src/Data/CodeWars/Kyu_7/mumbling/testCase.js
+++ JavaScript/src/Data/CodeWars/Kyu_7/mumbling/testCase.js
@@ -1,42 +1,136 @@
-const { assert } = require('chai');
+const Test = require('@codewars/test-compat');
 
-const getCount = () => {
-  console.log('test');
+const accum = () => {
+  console.log('Test');
 };
 
-describe('Vowels Count Tests', function () {
-  it("should return 5 for 'abracadabra'", function () {
-    assert.strictEqual(getCount('abracadabra'), 5);
-  });
-  it("should return 4 for 'pear tree'", function () {
-    assert.strictEqual(getCount('pear tree'), 4);
-  });
-  it("should return 13 for 'o a kak ushakov lil vo kashu kakao'", function () {
-    assert.strictEqual(getCount('o a kak ushakov lil vo kashu kakao'), 13);
-  });
-  it("should return 0 for 'my pyx'", function () {
-    assert.strictEqual(getCount('my pyx'), 0);
-  });
-  it('should return 168 for a long input', function () {
-    assert.strictEqual(
-      getCount(
-        'tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty'
-      ),
-      168
+describe('accum', function () {
+  it('Basic tests', function () {
+    Test.assertEquals(
+      accum('ZpglnRxqenU'),
+      'Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu'
+    );
+    Test.assertEquals(
+      accum('NyffsGeyylB'),
+      'N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb'
+    );
+    Test.assertEquals(
+      accum('MjtkuBovqrU'),
+      'M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu'
+    );
+    Test.assertEquals(
+      accum('EvidjUnokmM'),
+      'E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm'
+    );
+    Test.assertEquals(
+      accum('HbideVbxncC'),
+      'H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc'
+    );
+    Test.assertEquals(
+      accum('VwhvtHtrxfE'),
+      'V-Ww-Hhh-Vvvv-Ttttt-Hhhhhh-Ttttttt-Rrrrrrrr-Xxxxxxxxx-Ffffffffff-Eeeeeeeeeee'
+    );
+    Test.assertEquals(
+      accum('KurgiKmkphY'),
+      'K-Uu-Rrr-Gggg-Iiiii-Kkkkkk-Mmmmmmm-Kkkkkkkk-Ppppppppp-Hhhhhhhhhh-Yyyyyyyyyyy'
+    );
+    Test.assertEquals(
+      accum('NctlfBlnmfH'),
+      'N-Cc-Ttt-Llll-Fffff-Bbbbbb-Lllllll-Nnnnnnnn-Mmmmmmmmm-Ffffffffff-Hhhhhhhhhhh'
+    );
+    Test.assertEquals(
+      accum('WegunHvbdmV'),
+      'W-Ee-Ggg-Uuuu-Nnnnn-Hhhhhh-Vvvvvvv-Bbbbbbbb-Ddddddddd-Mmmmmmmmmm-Vvvvvvvvvvv'
+    );
+    Test.assertEquals(
+      accum('VoywwSpqidE'),
+      'V-Oo-Yyy-Wwww-Wwwww-Ssssss-Ppppppp-Qqqqqqqq-Iiiiiiiii-Dddddddddd-Eeeeeeeeeee'
+    );
+    Test.assertEquals(
+      accum('VbaixFpxdcO'),
+      'V-Bb-Aaa-Iiii-Xxxxx-Ffffff-Ppppppp-Xxxxxxxx-Ddddddddd-Cccccccccc-Ooooooooooo'
+    );
+    Test.assertEquals(
+      accum('OlyqvYwkuzF'),
+      'O-Ll-Yyy-Qqqq-Vvvvv-Yyyyyy-Wwwwwww-Kkkkkkkk-Uuuuuuuuu-Zzzzzzzzzz-Fffffffffff'
+    );
+    Test.assertEquals(
+      accum('JrhfdMtchiH'),
+      'J-Rr-Hhh-Ffff-Ddddd-Mmmmmm-Ttttttt-Cccccccc-Hhhhhhhhh-Iiiiiiiiii-Hhhhhhhhhhh'
+    );
+    Test.assertEquals(
+      accum('JiwpcSwslvW'),
+      'J-Ii-Www-Pppp-Ccccc-Ssssss-Wwwwwww-Ssssssss-Lllllllll-Vvvvvvvvvv-Wwwwwwwwwww'
+    );
+    Test.assertEquals(
+      accum('EagpiEvmabJ'),
+      'E-Aa-Ggg-Pppp-Iiiii-Eeeeee-Vvvvvvv-Mmmmmmmm-Aaaaaaaaa-Bbbbbbbbbb-Jjjjjjjjjjj'
+    );
+    Test.assertEquals(
+      accum('RznlcEmuxxP'),
+      'R-Zz-Nnn-Llll-Ccccc-Eeeeee-Mmmmmmm-Uuuuuuuu-Xxxxxxxxx-Xxxxxxxxxx-Ppppppppppp'
+    );
+    Test.assertEquals(
+      accum('OrggaExarzP'),
+      'O-Rr-Ggg-Gggg-Aaaaa-Eeeeee-Xxxxxxx-Aaaaaaaa-Rrrrrrrrr-Zzzzzzzzzz-Ppppppppppp'
+    );
+    Test.assertEquals(
+      accum('DriraMtedfB'),
+      'D-Rr-Iii-Rrrr-Aaaaa-Mmmmmm-Ttttttt-Eeeeeeee-Ddddddddd-Ffffffffff-Bbbbbbbbbbb'
+    );
+    Test.assertEquals(
+      accum('BjxseRxgtjT'),
+      'B-Jj-Xxx-Ssss-Eeeee-Rrrrrr-Xxxxxxx-Gggggggg-Ttttttttt-Jjjjjjjjjj-Ttttttttttt'
+    );
+    Test.assertEquals(
+      accum('EquhxOswchE'),
+      'E-Qq-Uuu-Hhhh-Xxxxx-Oooooo-Sssssss-Wwwwwwww-Ccccccccc-Hhhhhhhhhh-Eeeeeeeeeee'
     );
   });
-  it('should return correct results for random tests', function () {
-    const rnd = (m, n = 0) => (Math.random() * (n - m) + m) | 0;
-    const elements = (xs) => xs[rnd(xs.length)];
-    const CHARS = 'abcdefghijklmnopqrstuvwxyz    ';
-    const rndString = (size) =>
-      Array.from({ length: rnd(size) }, () => elements(CHARS)).join('');
-    const refGetCount = (s) => s.replace(/[^aeiou]/g, '').length;
-    for (let i = 1; i <= 100; i++) {
-      const s = rndString(i);
-      const actual = getCount(s);
-      const expected = refGetCount(s);
-      assert.strictEqual(actual, expected, `For input ${JSON.stringify(s)}`);
+});
+
+describe('Random tests', function () {
+  function randint(a, b) {
+    return Math.floor(Math.random() * (b - a + 1) + a);
+  }
+
+  //................
+  function accumSol109(s) {
+    function capitalize(s) {
+      return s.toLowerCase().replace(/\b./g, function (a) {
+        return a.toUpperCase();
+      });
     }
-  });
+    var a = s.split(''),
+      res = '';
+    for (var i = 0; i < a.length; i++) {
+      for (var j = 0; j < i + 1; j++) res += a[i];
+      res += '-';
+    }
+    return capitalize(res.substr(0, res.length - 1));
+  }
+  //................
+
+  function doEx() {
+    var i = 0;
+    var res = '';
+    while (i < 11) {
+      if (i % 5 === 0) var n = randint(65, 90);
+      else n = randint(97, 122);
+      res += String.fromCharCode(n);
+      i++;
+    }
+    return res;
+  }
+
+  for (var i = 0; i < 100; i++) {
+    var s1 = doEx();
+    it('Testing accum: ', function () {
+      Test.assertEquals(
+        accum(s1),
+        accumSol109(s1),
+        'It should work for random tests too'
+      );
+    });
+  }
 });
